a208d72611f7272be4b742da431e628501708adb
easai <codium@gmail.com>
2021-11-05 20:24:20 -0400

    Updated fi-quiz and added number of choices option.

--- src/fi-quiz.py
+++ src/fi-quiz.py
@@ -1,11 +1,12 @@
 """Finnish vocabulary quiz.
 
-Usage: fi-quiz.py [--help|-h] [--local|-l] [-n N]
+Usage: fi-quiz.py [--help|-h] [--local|-l] [-n N] [-m M]
 
 Options:
     -h, --help  show this help message and exit
     -l, --local  run locally
     -n N    number of tests (default: 10)
+    -m M    number of choices (default: 5)
 """
 import requests
 import platform
@@ -17,7 +18,7 @@ ossystem = platform.uname()[0]
 url = "https://sanakirja.pythonanywhere.com"
 
 
-def quiz(n=10, n_choices=3):
+def quiz(n=10, n_choices=5):
     try:
         n_words = int(n)
         for i in range(n_words):
@@ -46,11 +47,17 @@ def quiz(n=10, n_choices=3):
 if __name__ == "__main__":
     args = docopt(__doc__)
     n = 10
+    m = 5
     if args["-n"]:
         try:
             n = int(args['-n'])
         except ValueError:
             print("n must be an integer")
+    if args['-m']:
+        try:
+            m = int(args['-m'])
+        except ValueError:
+            print("m must be an integer")
     if args["--local"]:
         url = "http://localhost:5000"
-    quiz(n)
+    quiz(n, m)
